0fc037b8b506c4189e544e2197c95acd8b1508f4
Natalya <natalya.pa2016@gmail.com>
2020-12-06 21:56:53 +0300

    Added sending

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from flask_cors import CORS
+from flask_cors import CORS
 from datetime import datetime
 from flask_socketio import SocketIO
 
@@ -10,9 +10,12 @@ import ssl
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '88024e1b4d3d7a4d7c2f839092334feb1d4c8c36'
 socket_io = SocketIO(app, cors_allowed_origins="*")
-# CORS(app)
+CORS(app)
 app.config['JSON_AS_ASCII'] = False
 
+
+# to_id = "5fc23d1346ea132fe3066973"
+# from_id = "5fc2387539dc49f806f755c5"
 
 @app.route('/')
 @app.route('/index')
@@ -29,11 +32,12 @@ def connect_to_database():
 
 
 db = connect_to_database()
+date = datetime.now()
+print(date)
 
 
-
-def get_users():
-    cursor = db.user.find({})
+def get_correspondents(from_id):
+    cursor = db.user.find({"from_id": from_id})
     result = []
     for item in cursor:
         item["_id"] = str(item["_id"])
@@ -50,6 +54,7 @@ def auth():
     print(get_password_by_login("natalya"))
     print(get_password_by_login("kur"))
 
+
 def get_password_by_login(login):
     cursor = db.user.find({"login": login})
     password = None
@@ -60,15 +65,35 @@ def get_password_by_login(login):
         return -1
     return password
 
-def get_messages_from_chat(to_id, from_id):
-    cursor = db.user.find({"to_id": ObjectId(to_id), "from_id": ObjectId(from_id)})
+
+@app.route("/messages")
+def get_messages_from_chat():
+    cursor = db.message.find(
+        {"$or": [
+            {"to_id": "5fc23d1346ea132fe3066973", "from_id": "5fc2387539dc49f806f755c5"},
+            {"to_id": "5fc2387539dc49f806f755c5", "from_id": "5fc23d1346ea132fe3066973"}
+        ]
+        })
+    result = []
+    for item in cursor:
+        item["_id"] = str(item["_id"])
+        result.append(item)
+    return jsonify(result)
 
 
 @app.route("/send", methods=['POST'])
-def send_message(from_id, to_id, text):
-    ''', "time": datetime.now'''
-    db.message.insert_one({ "from_id": from_id, "to_id": to_id, "text": text})
+def send_message():
+    data = request.get_json()
+    from_id = data['from_id']
+    to_id = data['to_id']
+    text = data['text']
+    date = datetime.now()
+    db.message.insert_one(
+        {"from_id": from_id, "to_id": to_id, "text": text, "date": date})
+    print("All right guys everything is fine")
     socket_io.emit('message', get_messages_from_chat(to_id, from_id))
+    return {'response': 'ok'}
+
 
 
 app.run()
